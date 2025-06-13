20246efdedcbbd98854c15d705464e7a636c4990
Shivnath Tathe <shivnath@isgesolutions.com>
2025-06-13 14:13:59 +0530

    Changes in angular pulseHI

--- src/environment/environmant.ts
+++ src/environment/environmant.ts
@@ -3,4 +3,4 @@ export const environment = {
     chatUrl: 'https://dhruvkbapi.isgesolutions.com/api/v1/holidayidea/recommend',
     redirectDomain:'https://angular-mydhruv.vercel.app',
     EncryDecryUrl:'https://online.wildfrontierstravel.com/mydhruvapi/api/v2/account'
-}
+}   
