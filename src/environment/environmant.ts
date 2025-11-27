0a91cf3b8b7bed41f2eff48ca0cbb592351abb8f
DevkarGanesh <ganesh@isgesolutions.com>
2025-11-27 14:48:16 +0530

    added new url

--- src/environment/environmant.ts
+++ src/environment/environmant.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    chatUrl: 'https://pills-screen-arc-cited.trycloudflare.com',
+    chatUrl: 'https://dhruvkbapitest.isgesolutions.com:8003',
     query: "/query",
     // redirectDomain:'https://angular-mydhruv.vercel.app',
     // EncryDecryUrl:'https://online.wildfrontierstravel.com/mydhruvapi/api/v2/account'
